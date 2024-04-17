ffeacbb5a819410844d81e22f6b1392ad3306bc7
Dabaghi-Anass <anass.dabaghi123@gmail.com>
2024-04-17 13:19:14 +0100

    modification

---
+++ src/components/medical-information.tsx
@@ -0,0 +1,28 @@
+import { HeartPulse } from "lucide-react";
+
+function randomHsl() {
+  const hue = Math.floor(Math.random() * 360);
+  return `hsl(${hue},46%,42%)`
+}
+export default function MedicalInformation({ patient, className }: { patient: any, className: string }) {
+  console.log(patient)
+  return <div className={className}>
+    {patient?.medicalInformation?.bloodType &&
+      <div className="flex bg-rose-600 gap-2 font-bold text-white w-fit px-2 py-1 rounded-full">
+        <HeartPulse />
+        <h3>blood type : <span className="text-yellow-400 mx-2">{patient?.medicalInformation?.bloodType}</span></h3>
+      </div>
+    }
+    {patient?.medicalInformation?.allergies &&
+      <>
+        <h1 className="text-2xl font-semibold my-8">Allergies</h1>
+        <div className="flex flex-wrap gap-4 items-center text-secondary-foreground">
+          {patient.medicalInformation.allergies.split(",").map((a: string) => (
+            <div className="flex gap-2 font-semibold w-fit px-2 py-1 rounded-full" style={{
+              background: randomHsl()
+            }}>{a}</div>
+          ))}
+        </div>
+      </>}
+  </div>
+}
--- src/components/treatment-request-details.tsx
+++ src/components/treatment-request-details.tsx
@@ -1,3 +1,4 @@
+import MedicalInformation from "./medical-information";
 import ProfileHeader from "./profile-header";
 type Props = {
   request: any | null;
@@ -9,18 +10,16 @@ export default function TreatmentRequestDetails({ request }: Props) {
     </section>
   return <section className="profile bg-primary-foreground rounded-lg with-border flex flex-col gap-8 w-full p-6">
     <ProfileHeader user={request.sentBy} />
-    <div className="w-full h-full">
+    <div className="w-full">
       <div className="flex gap-2 mb-4 items-center">
         <h1 className="text-xl font-semibold">{request.title}</h1>
         <span className="status-badge lowercase" style={{
-          border: "1px solid orangered",
-          color: "orangered"
+          border: `1px solid ${request.status === "DONE" ? "#34eb86" : "orange"}`,
+          color: request.status === "DONE" ? "#34eb86" : "orange"
         }}>{request.status}</span>
       </div>
       <p className="max-w-prose">{request.description}</p>
     </div>
-    <div className="medical-info">
-
-    </div>
+    <MedicalInformation className='medical-info' patient={request.sentBy} />
   </section>
 }
