065b5e48da1f13d93433064657ad28785a14f05f
Dabaghi-Anass <anass.dabaghi123@gmail.com>
2024-05-04 19:38:30 +0100

    adjustments

--- src/components/appointments-list.tsx
+++ src/components/appointments-list.tsx
@@ -3,7 +3,7 @@ import { useAppSelector } from "@/hooks/redux-hooks";
 import { getStatusHue } from "@/lib/utils/utils";
 import Link from "next/link";
 
-export default function ProfileAppointementSchedule({ inModal, appointments }: { inModal?: boolean, appointments: any[] }) {
+export default function ProfileAppointementSchedule({ inModal, isInProfilePage, appointments }: { inModal?: boolean, isInProfilePage?: boolean, appointments: any[] }) {
   const currentUser: any = useAppSelector(state => state.UserReducer.user)
   if (!currentUser) return null
   return <div className={`p-4 with-border flex flex-col gap-4 rounded-lg w-full h-full hide-footer lg:overflow-hidden sm:row-span-1 lg:row-span-full ${inModal ? "lg:col-span-2" : ""}`}>
@@ -13,6 +13,6 @@ export default function ProfileAppointementSchedule({ inModal, appointments }: {
         .toSorted((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
         .map((appointment) => <SmallAppointment showDoctorName={appointment.assignedTo.id === currentUser.id} key={appointment.id} color={`hsl(${getStatusHue(appointment.status)}, 100%, 74%)`} appointment={appointment} />)}
     </div>
-    <Link href="/appointments" className="z-[200] w-full text-center link">see all</Link>
+    <Link href={`/appointments/${isInProfilePage ? "" : appointments[0]?.patient.id}`} className="z-[200] w-full text-center link">see all</Link>
   </div>
 }
