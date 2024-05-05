6375627a2b891322c130def287b0e32101a8b941
Dabaghi-Anass <anass.dabaghi123@gmail.com>
2024-05-05 21:36:48 +0100

    some fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client"
+import { useAppSelector } from "@/hooks/redux-hooks";
 import { useRouter } from "next/navigation";
 export default function Home() {
 	const router = useRouter();
-	router.replace("/calendrier")
+	const user: any = useAppSelector((state) => state.UserReducer.user);
+	if (user?.role === "CAREGIVER") router.replace("/agendas")
+	else router.replace("/calendrier")
 	return <div className="w-full h-full grid place-content-center">Redirecting...</div>;
 }
--- src/components/ui/small-appointment-visualizer.tsx
+++ src/components/ui/small-appointment-visualizer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Badge } from "./badge";
 
 type Props = {
   appointment: any;
@@ -9,11 +10,12 @@ type Props = {
 }
 export default function SmallAppointment({ appointment, showDoctorName, showCaregiverName, color, onClick }: Props) {
   let name;
-  if (appointment.assignedto) {
+  if (appointment.assignedTo) {
     name = showDoctorName ? `${appointment.assignedTo.lastName.charAt(0)}.${appointment.assignedTo.firstName}` : `${appointment.patient.lastName.charAt(0)}.${appointment.patient.firstName}`;
   } else if (appointment.caregiver) {
     name = showCaregiverName ? `${appointment.caregiver.lastName.charAt(0)}.${appointment.caregiver.firstName}` : `${appointment.patient.lastName.charAt(0)}.${appointment.patient.firstName}`;
   }
+  let role = appointment.assignedTo?.role || appointment.caregiver?.role;
   return <Link href={`${appointment.description ? "/activities/activity" : "/appointments/appointment"}/${appointment.id}`}>
     <div className="flex flex-col items-start gap-2 hover:opacity-55">
       <div className="text-slate-500 text-xs flex items-center ">
@@ -25,7 +27,9 @@ export default function SmallAppointment({ appointment, showDoctorName, showCare
       </div>
 
       <div className="flex flex-col w-full with-border p-4 rounded-lg">
-        <span className="font-semibold capitalize text-slate-700 dark:text-slate-300 text-sm flex items-center gap-2">{name} <span className="status-badge lowercase" style={{ color, border: "1px solid " + color }}>{appointment.status || "en attente"}</span></span>
+        <span className="font-semibold capitalize text-slate-700 dark:text-slate-300 text-sm flex items-center gap-2 w-full justify-between"><div className="span flex gap-2">{name} <Badge className="rounded-lg">{role}</Badge></div>
+          <span className="status-badge lowercase" style={{ color, border: "1px solid " + color }}>{appointment.status || "en attente"}</span>
+        </span>
         <div className="flex items-center gap-2">
           <span className="w-4 h-4 appointment-status rounded shadow-md inline-block" style={{ background: color }}></span>
           <span className="text-slate-400">{appointment.type}</span>
