f633df9fcfe276df899a6ad81e44a1741bc5037a
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-07-21 20:08:34 +0100

    added open-graph meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,35 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Gestion Des Soins",
   description: "application de gestion des soins",
+  openGraph: {
+    title: "hopital de fsdm",
+    description: "Il s'agit d'une application créée pour aider les professionnels de la santé à communiquer facilement avec les clients",
+    type: "website",
+    locale: "fr_FR",
+    url: "https://www.fsdm-hospital.me",
+    siteName: "Gestion Des Soins",
+    images: [
+      {
+        url: "https://img.freepik.com/photos-gratuite/portrait-medecin-videoconference-clinique_23-2148728382.jpg?t=st=1721588306~exp=1721591906~hmac=422b55dda9886d25c9bbf28d7f5866c580a50fa8f9c1b17d2add8f3f86abd26b&w=740",
+        width: 800,
+        height: 600,
+        alt: "Gestion Des Soins",
+      },
+      {
+        url: "https://img.freepik.com/photos-gratuite/docteur-verifiant-ses-patients_23-2149017270.jpg?uid=R35211599&ga=GA1.1.2001347315.1721587973&semt=sph",
+        width: 800,
+        height: 600,
+        alt: "Gestion Des Soins",
+      },
+      {
+        url: "https://img.freepik.com/vecteurs-libre/chirurgiens-faisant-illustration-concept-chirurgie_114360-15650.jpg?t=st=1721588698~exp=1721592298~hmac=b958ce62ca82c9d4e8c9ebde0f0a5e9f108fb7831ad7db0e427c97e45f6c4459&w=740",
+        width: 800,
+        height: 600,
+        alt: "Gestion Des Soins",
+      },
+
+    ],
+  }
 };
 
 
